56ad8ad53c20497fde2f1a7664f7d7cb9f4a0d2e
snaCW <a.navid2505@gmail.com>
2024-09-01 14:05:29 +0330

    Added quickMenu

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const sideMenu = document.getElementById("sideMenu");
+const quickMenu = document.getElementById("quickMenu");
 
 async function fetchJSONData(path) {
     const responce = await fetch(path);
@@ -6,37 +7,74 @@ async function fetchJSONData(path) {
     return json;
 }
 
-async function loadSideNavOptions() {
+async function loadPagesLinks() {
     sideMenu.innerHTML = `<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>`;
-    const pages = await fetchJSONData("pages.json");
+    const pages = JSON.parse(localStorage.getItem("pages"));
+    pages.sort((a, b) => b.UsageCount - a.UsageCount);
+
+    let pagesAddedToQuickMenu = 0;
     pages.forEach(page => {
-        sideMenu.innerHTML += `<a href="${page.Path}" onclick="${page}.UsageCount++">${page.Name}</a>`;
+        sideMenu.innerHTML += `<a href="${page.Path}" class="sideMenuButton" onclick="increasePageUsageCount(this)">${page.Name}</a>`;
+
+        if (pagesAddedToQuickMenu < 4) {
+            quickMenu.innerHTML += `<a href="${page.Path}" class="quickMenuButton" onclick="increasePageUsageCount(this)">${page.Name}</a>`;
+            pagesAddedToQuickMenu++;
+        }
     });
 }
 
-async function openNav() {
-    await loadSideNavOptions();
-    document.getElementById("sideMenu").style.width = "250px";
+function openNav() {
+    sideMenu.style.width = "250px";
 }
 
 function closeNav() {
-    document.getElementById("sideMenu").style.width = "0px";
+    sideMenu.style.width = "0px";
 }
 
-function updateLocalStoragePages {
+async function updateLocalStoragePages() {
     const jsonPages = await fetchJSONData("pages.json");
     if (localStorage.getItem("pages") === null) {
         localStorage.setItem("pages", JSON.stringify(jsonPages));
         return;
     }
     
-    const localStoragePages = JSON.parse(localStorage.getItem("pages"));
-    for (int i = 0; i < jsonPages.length(); i++) {
-        
+    let localStoragePages = JSON.parse(localStorage.getItem("pages"));
+
+    let indexesToAdd = [];
+    for (let i = 0; i < jsonPages.length(); i++) {
+        let isPageFound = false;
+
+        for (let j = 0; j < localStoragePages.length(); j++) {
+            if (jsonPages[i].Name == localStoragePages[j].Name) {
+                isPageFound = true;
+
+                localStoragePages[j].Path = jsonPages[i].Path;
+                break;
+            }
+        }
+
+        if (!isPageFound) {
+            indexesToAdd.push(i);
+        }
     }
     
-    jsonPages.forEach(jsonPage => {
-        let pageFound = false;
-        localStoragePages.forEach
+    indexesToAdd.forEach(index => {
+        localStoragePages.push(jsonPages[index]);
     });
+    localStorage.setItem("pages", JSON.stringify(localStoragePages));
+}
+
+async function increasePageUsageCount(pageClicked) {
+    const pages = JSON.parse(localStorage.getItem("pages"));
+    for (let i = 0; i < pages.length(); i++) {
+        if (pages[i].Name == pageClicked.innerText) {
+            pages[i].UsageCount++;
+            break;
+        }
+    }
+    localStorage.setItem("pages", JSON.stringify(localStoragePages));
+}
+
+async function resetPagesUsageCount() {
+    localStorage.setItem("pages", JSON.stringify(await fetchJSONData("pages.json")));
 }
